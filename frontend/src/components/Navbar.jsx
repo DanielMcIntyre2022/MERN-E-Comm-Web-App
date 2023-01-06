24024a06698d17edb000034057e68a58de5fe889
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-06 15:37:08 -0500

    created navbar component and UI for it

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,33 @@
+import SearchIcon from '@mui/icons-material/Search';
+import { Badge } from '@mui/material';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 function Navbar() {
   return (
-    <div className='text-3xl font-bold underline'>
-        Navbar
+    <div className='nav-container h-20'>
+        <div className="wrapper flex justify-between items-center pt-10 pb-10 px-20">
+        <div className="left-container flex items-center flex-1">
+              <div className="language-container cursor-pointer text-sm p-2.5">EN</div>
+              <div className="search-bar-container flex items-center border p-2.5">
+                <input className='border-none'/>
+                <i><SearchIcon/></i>
+              </div>
+           </div>
+           <div className="center-container text-center flex-1">
+            <h1 className='font-bold'>E-Commerce</h1>
+           </div>
+           <div className="right-container flex items-center justify-end flex-1">
+              <div className='menu-item-container flex space-x-6'>
+                <div className='menu-item-one'>REGISTER</div>
+                <div className='menu-item-two'>SIGN IN</div>
+                <div className='menu-item-three'>
+                <Badge badgeContent={4} color="primary">
+                  <ShoppingCartOutlinedIcon color="action" />
+               </Badge>
+              </div>
+             </div>
+           </div>
+      </div>
     </div>
   )
 }
