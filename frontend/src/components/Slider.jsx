a245b7f664c7de6d26860aef154d0ff33b04d945
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-09 14:43:29 -0500

    added responsive design for slider component

--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -2,7 +2,7 @@
 
 function Slider() {
   return (
-    <div className="slider-container w-100% h-screen flex bg-yellow-200 relative mt-6">
+    <div className="slider-container w-100% h-screen flex bg-yellow-200 relative mt-6 max-sm:hidden">
         Slider
     </div>
   )
