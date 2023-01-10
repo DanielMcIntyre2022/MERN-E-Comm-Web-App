e39450bfa1b597cabd1daac30d2b3ee89ccd7c45
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-10 10:02:43 -0500

    created login auth route function

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -18,4 +18,21 @@ router.post('/register', async (req, res) => {
     }
 });
 
+// LOGIN //
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        !user && res.status(401).json('wrong credentials!')
+
+        const hashedPassword = Cryptojs.AES.decrypt(user.password, process.env.PASS_SEC)
+        const password = hashedPassword.toString(Cryptojs.enc.Utf8);
+
+        password !== req.body.password && res.status(401).json('wrong credentials!')
+        res.status(200).json(user);
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 module.exports = router;
