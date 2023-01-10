49e449dca941d44c054d614d5f2b5a108aa36d0f
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-10 10:45:26 -0500

    created delete user function in users route file

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,4 @@
-
+const User = require('../models/User');
 const {vertifyToken, verifyTokenAndAuthorization} = require('./verifyToken');
 const router = require('express').Router();
 
@@ -15,8 +15,19 @@ router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
             $set: req.body,
         }, {new:true});
         res.status(200).json(upatedUser);
-    } catch (error){res.status(500).json(err)} {
-        
+    } catch (error){res.status(500).json(error)} {
+        console.log(error)
+    }
+});
+
+
+// DELETE //
+router.delete('/:id', verifyTokenAndAuthorization, async(req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json('user has been deleted')
+    } catch (error) {
+        res.status(500).json(error)
     }
 });
 
