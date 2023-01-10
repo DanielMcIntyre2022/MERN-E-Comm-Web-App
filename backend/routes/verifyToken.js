9fc70af87397d6d6c2a8335f122aa08a4153b3f1
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-10 10:40:12 -0500

    updated verifyToken function to include bearer

--- backend/routes/verifyToken.js
+++ backend/routes/verifyToken.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const vertifyToken = (req, res, next) => {
     const authHeader = req.headers.token
     if(authHeader) {
+        const token = authHeader.split(' ')[1];
         jwt.verify(token, process.env.JWT_SEC, (err, user) => {
             if(err) res.status(403).json('Token is not valid!')
             req.user = user;
