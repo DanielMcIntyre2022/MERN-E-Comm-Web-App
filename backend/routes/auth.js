8a47200a811f13002f07b0cfb93e3502334fdc0c
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-10 09:53:27 -0500

    added password encryption in auth route funtion

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,19 +1,20 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const Cryptojs = require('crypto-js');
 
 // REGISTER //
 router.post('/register', async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: Cryptojs.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
     });
     try {
         const savedUser = await newUser.save();
         res.status(201).json(savedUser)
     } catch (error) {
         console.log(error)
-        res.status(500).json(err);
+        res.status(500).json(error);
     }
 });
 
