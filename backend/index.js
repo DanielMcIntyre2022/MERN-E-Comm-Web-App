c6651c603368cfcbca5c49bd14a9ae12483fee90
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-12 13:09:30 -0500

    imported route functions in main index file

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT;
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('DB Connection Successful'))
@@ -19,6 +21,8 @@ app.use(express.json());
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
+app.use('/api/cart', cartRoute);
+app.use('/api/order', orderRoute);
 
 app.listen(PORT, () => {
     console.log(`server is running! on port ${PORT}`)
