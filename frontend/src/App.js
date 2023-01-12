153819d696a25df4d5e87ef4bc32419082bd879b
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-12 16:26:37 -0500

    react-routersetup, and included payments route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,18 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import ProductList from './pages/ProductList';
 import Product from "./pages/Product";
+import Payment from './pages/Payment';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Cart/>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>  
+            <Route path='/payments' element={<Payment/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
