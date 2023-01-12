1e5924876136d7a4ebd5f0b7f713e75f61a018ec
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-12 16:43:39 -0500

    added paysuccess route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Login from './pages/Login';
 import ProductList from './pages/ProductList';
 import Product from "./pages/Product";
 import Payment from './pages/Payment';
+import PaySuccess from './pages/PaySuccess';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -13,10 +14,11 @@ function App() {
       <div className="App">
         <Routes>  
             <Route path='/payments' element={<Payment/>}/>
+            <Route path='/paysuccess' element={<PaySuccess/>}/>
         </Routes>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/pages/PaySuccess.jsx
@@ -0,0 +1,8 @@
+
+function PaySuccess() {
+  return (
+    <div>PaySuccess</div>
+  )
+}
+
+export default PaySuccess;
