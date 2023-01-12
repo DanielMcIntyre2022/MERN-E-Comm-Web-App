59c7fadb577671ffdc09c10a78eb44a9415c7f42
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-12 16:13:22 -0500

    imported stripRoute in main index.js file

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,8 @@ const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
+const stripeRoute = require('./routes/stripe');
+const cors = require('cors');
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('DB Connection Successful'))
@@ -18,11 +20,13 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use(express.json());
 
+app.use(cors());
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 app.use('/api/cart', cartRoute);
 app.use('/api/orders', orderRoute);
+app.use('/api/checkout', stripeRoute);
 
 app.listen(PORT, () => {
     console.log(`server is running! on port ${PORT}`)
