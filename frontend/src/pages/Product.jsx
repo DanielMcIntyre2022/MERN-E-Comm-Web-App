76833ae01e6dd3ac0e9917b398ca3417dfa333ef
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-17 14:17:05 -0500

    utilized dot notation and map method to display data related to single product

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,6 +1,6 @@
 import Annoucements from "../components/Annoucements";
 import Navbar from "../components/Navbar";
-import NewsLetter from '../components/NewsLetter';
+import NewsLetter from "../components/NewsLetter";
 import Footer from "../components/Footer";
 import axios from "axios";
 import { useLocation } from 'react-router-dom';
@@ -16,13 +16,13 @@ function Product() {
     useEffect(() => {
         const getProduct = async () => {
             try {
-                const response = publicRequest.get('/products/find'+productId);
+                const response = await publicRequest.get('/products/find/' + productId);
                 setProduct(response.data);
             } catch (error) {
-            getProduct()
             }
         }
-    },[]);
+        getProduct();
+    },[productId]);
 
   return (
     <div className="product-page-container">
@@ -30,15 +30,12 @@ function Product() {
         <Navbar/>
             <div className="product-wrapper p-50 flex mt-10 mb-10 max-sm:flex-col">
                 <div className="img-container flex-1">
-                    <img className="w-100% object-cover ml-10 max-sm:w-70% max-sm:mr-10 max-sm:pl-12" src="https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80" alt="" />
+                    <img className="w-100% object-cover ml-10 max-sm:w-70% max-sm:mr-10 max-sm:pl-12" src={product.img} alt="" />
                 </div>
                 <div className="info-container flex-1 pt-2 pb-2 pl-20 pr-20">
-                    <h1 className="font-semibold">Winter Sweater</h1>
-                    <p className="mt-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, 
-                        veritatis porro commodi, corrupti natus, et consequatur mollitia 
-                        cumque perferendis doloremque laudantium! Officia fuga itaque natus 
-                        placeat quam veniam labore suscipit.</p>
-                    <p className="mt-5">$20</p>
+                    <h1 className="font-semibold">{product.title}</h1>
+                    <p className="mt-5">{product.description}</p>
+                    <p className="mt-5">${product.price}</p>
                     <div className="filter-container flex justify-between w-50% mt-10 mb-10 max-sm:w-100%">
                         <div className="filter-one">
                             <h1>Color</h1>
@@ -47,11 +44,9 @@ function Product() {
                         <div className="filter-two">
                             <h1>Size</h1>
                             <select className="p-2 mr-5">
-                                <option>XS</option>
-                                <option>S</option>
-                                <option>M</option>
-                                <option>L</option>
-                                <option>XL</option>
+                                {product.size.map((s) => (
+                                     <option key={s}>{s}</option>
+                                ))}
                             </select>
                         </div>
                     </div>
