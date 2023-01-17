e740b24f64245fa0c552aa431c51bdcd82e9149a
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-17 11:13:48 -0500

    updates to routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,10 @@ import ProductList from './pages/ProductList';
 import Product from "./pages/Product";
 import Payment from './pages/Payment';
 import PaySuccess from './pages/PaySuccess';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
+  const user = true;
   return (
     <Router>
       <div className="App">
@@ -17,7 +18,7 @@ function App() {
             <Route path='/products/:category' element={<ProductList/>}/>
             <Route path='/product/:id' element={<Product/>}/>
             <Route path='/cart' element={<Cart/>}/>
-            <Route path='/login' element={<Login/>}/>
+            <Route path='/login' element={user ? <Navigate to='/'/> :<Login/>}/>
             <Route path='/register' element={<Register/>}/>
             <Route path='/payments' element={<Payment/>}/>
             <Route path='/paysuccess' element={<PaySuccess/>}/>
