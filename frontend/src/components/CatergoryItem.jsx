d01e0a08b37cdc43f877e62f7895be9e68378064
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-17 11:25:30 -0500

    linked to caterogy item data in catergoryItem component

--- frontend/src/components/CatergoryItem.jsx
+++ frontend/src/components/CatergoryItem.jsx
@@ -1,16 +1,18 @@
 
+import { Link } from 'react-router-dom';
 
 function CatergoryItem({item}) {
   return (
     <div className="catergory-item-container flex-1 m-3 h-16 -mt-10 relative">
-        <div className="catergory-image-container">
-            <img className="w-100% h-80 object-cover" src={item.img}/>
-        </div>
-        <div className="catergory-info-container absolute w-100% h-full top-0 
-        left-0 flex items-center justify-center flex-col ml-5 mt-5 font-bold">
-            <p className="text-white mb-1">{item.title}</p>
-            <button className="border-4 text-white p-2">SHOP NOW</button>
-        </div>
+        <Link to={`/products/${item.catergory}`}>
+            <div className="catergory-image-container">
+                <img className="w-100% h-80 object-cover" src={item.img}/>
+            </div>
+            <div className="catergory-info-container absolute w-100% h-full top-0 left-0 flex items-center justify-center flex-col ml-5 mt-5 font-bold">
+                <p className="text-white mb-1">{item.title}</p>
+                <button className="border-4 text-white p-2">SHOP NOW</button>
+            </div>
+        </Link>
     </div>
   )
 }
