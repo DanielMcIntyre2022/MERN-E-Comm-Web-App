a6f4d67f8c76c7ead09168599c483f67a32c5c67
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-17 17:07:29 -0500

    created login form validation error handling and redirected user to home page once authenticated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
+
 import Cart from "./pages/Cart";
 import Home from './pages/Home';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import ProductList from './pages/ProductList';
+import Login from "./pages/Login";
+import Register from './pages/Register'
+import ProductList from "./pages/ProductList";
 import Product from "./pages/Product";
 import Payment from './pages/Payment';
 import PaySuccess from './pages/PaySuccess';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
 
 function App() {
-  const user = false;
+  const user = useSelector(state => state.user.currentUser);
   return (
     <Router>
       <div className="App">
@@ -18,14 +20,14 @@ function App() {
             <Route path='/products/:category' element={<ProductList/>}/>
             <Route path='/product/:id' element={<Product/>}/>
             <Route path='/cart' element={<Cart/>}/>
-            <Route path='/login' element={user ? <Navigate to='/'/> :<Login/>}/>
-            <Route path='/register' element={user ? <Navigate to='/'/> :<Register/>}/>
-            <Route path='/payments' element={<Payment/>}/>
+            <Route path='/login' element={user ? <Navigate to='/'/> : <Login/>}/>
+            <Route path='/register' element={user ? <Navigate to='/'/> : <Register/>}/>
+            <Route path='/payment' element={<Payment/>}/>
             <Route path='/paysuccess' element={<PaySuccess/>}/>
         </Routes>
       </div>
     </Router>
   );
-};
+}
 
 export default App;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,5 @@
-
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { login } from '../redux/apiCalls';
 
 function Login() {
@@ -8,6 +7,7 @@ function Login() {
   const [ username, setUserName ] = useState('');
   const [ password, setPassword ] = useState('');
   const dispatch = useDispatch();
+  const { isFetching, error } = useSelector((state) => state.user);
 
   const handleClick = (e) => {
     e.preventDefault();
@@ -21,7 +21,10 @@ function Login() {
             <form className="flex flex-col">
                 <input className="border flex-1 mb-3 mt-3 p-2" placeholder="username" onChange={(e) => setUserName(e.target.value)}/>
                 <input className="border flex-1 mb-3 mt-3 p-2" placeholder="password" type="password" onChange={(e) => setPassword(e.target.value)}/>
-                <button className="border p-2" onClick={handleClick}>LOGIN</button>
+                <button className="border p-2" onClick={handleClick} disabled={isFetching}>LOGIN</button>
+               {
+                error && <span className='text-red-400'>Something went wrong...</span>
+               }
                 <a className="mt-5 mb-5 cursor-pointer">Forgot password?</a>
                 <a className="cursor-pointer">Create a new account</a>
             </form>
