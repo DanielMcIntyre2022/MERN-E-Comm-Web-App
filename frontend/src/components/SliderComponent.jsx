fd6fab56bc0e5f8e84cf08da932e3671512d546e
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-18 12:22:32 -0500

    updates to SliderComponent

--- frontend/src/components/SliderComponent.jsx
+++ frontend/src/components/SliderComponent.jsx
@@ -18,24 +18,26 @@ function SliderComponent() {
 
     return (
     <div className='slider-wrapper w-100% max-sm:hidden'>
-            <div className='slider-container mt-10'>
+        <div className='slider-container mt-10'>
                 <Slider {...settings}>
                 {sliderItems.map(item=> (
                 <div className={`slider-one ${item.bg}`}>
-                      <div className='img-container flex-1 h-full ml-20'>
+                    <div className='slider-content-container flex'>
+                      <div className='img-container h-full ml-20'>
                           <img className="h-[80vh]" src={item.img} alt="falljacket"></img>
                       </div>
-                  <div className='info-container flex-1'>
+                  <div className='info-container'>
                       <h1 className='text-7xl'>{item.title}</h1>
                       <p className='text-lg mt-10 mb-10 font-medium tracking-wide'>{item.description}</p>
                       <button className='p-5 text-xl bg-transparent border-4'>Shop now</button>
-                      </div>
-                  </div>
-                ))}
-                </Slider>
+                </div>
             </div>
         </div>
-      )
+                ))}
+            </Slider>
+        </div>
+    </div>
+    )
 }
 
 export default SliderComponent;
