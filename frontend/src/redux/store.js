a60101c6be055654ae0ac21163f579a7075adf96
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-18 10:39:34 -0500

    utilized redux-persist in order to keep the user logged in after page refresh and prevented reaching the login page again

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,38 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from './cartRedux';
 import userReducer from './userRedux';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 
-export default configureStore({
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+  };
+
+  const persistedReducer = persistReducer(persistConfig, userReducer)
+
+export const store = configureStore({
     reducer:{
         cart: cartReducer,
-        user: userReducer
-    }
+        user: persistedReducer
+    },
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export let persistor = persistStore(store);
