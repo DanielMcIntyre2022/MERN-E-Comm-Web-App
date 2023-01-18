1aa36f79fa23a1ebf95efebce5ccd2927b891343
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-18 13:50:55 -0500

    created static admin data for testing

---
+++ admin/src/data.js
@@ -0,0 +1,27 @@
+
+export const productRows = [
+    { 
+        id: 1, 
+        name: 'Winter Sweater', 
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfrCRZY-C1hrAm9VlqOPjWkN5vFRYvqCShRg&usqp=CAU',
+        stock: 123,
+        status: 'active',
+        price: '$120'
+    },
+    { 
+        id: 2, 
+        name: 'Winter Sweater', 
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfrCRZY-C1hrAm9VlqOPjWkN5vFRYvqCShRg&usqp=CAU',
+        stock: 123,
+        status: 'active',
+        price: '$120'
+    },
+    { 
+        id: 3, 
+        name: 'Winter Sweater', 
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfrCRZY-C1hrAm9VlqOPjWkN5vFRYvqCShRg&usqp=CAU',
+        stock: 123,
+        status: 'active',
+        price: '$120'
+    },
+  ];
