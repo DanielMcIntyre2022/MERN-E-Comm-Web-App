fed3c8cef172c171359fef46dc8c139877e580b2
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-19 11:01:33 -0500

    created redux folder and created/exported productReduxslice file

---
+++ admin/src/redux/productRedux.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const productslice = createSlice({
+    name: 'product',
+    initialState: {
+        products: [],
+        isFetching: false, 
+        error: false
+    },
+    reducers: {
+        // Get all Products //
+        getProductStart:(state) => {
+            state.isFetching = true;
+            state.error = false;
+        },
+        getProductSuccess:(state, action) => {
+            state.isFetching = false;
+            state.products = action.payload;
+        },
+        getProductFailure:(state) => {
+            state.error = true;
+        }
+    },
+});
+
+export const {
+    getProductStart, getProductSuccess, getProductFailure
+} = productslice.actions;
