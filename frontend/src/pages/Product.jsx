58559a9c24b9a32983b21925d7c0d6cc942acc02
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-20 19:16:33 -0500

    minor style additions to Product component

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -44,16 +44,16 @@ function Product() {
     <div className="product-page-container">
         <Annoucements/>
         <Navbar/>
-            <div className="product-wrapper p-50 flex mt-10 mb-10 max-sm:flex-col">
-                <div className="img-container flex-1">
-                    <img className="w-100% object-cover ml-10 max-sm:w-70% max-sm:mr-10 max-sm:pl-12" src={product.img} alt="" />
+            <div className="product-wrapper p-50 flex mt-10 mb-10 max-sm:flex-col justify-center items-center m-auto">
+                <div className="img-container -ml-10">
+                    <img className="w-100% h-80 object-cover ml-20 max-sm:w-70% max-sm:mr-10 max-sm:pl-12" src={product.img} alt="" />
                 </div>
-                <div className="info-container flex-1 pt-2 pb-2 pl-20 pr-20">
+                <div className="info-container t-2 pb-2 pl-20 pr-20 ml-20">
                     <h1 className="font-semibold">{product.title}</h1>
                     <p className="mt-5">${product.price}</p>
-                    <div className="filter-container flex justify-between w-50% mt-10 mb-10 max-sm:w-100%">
+                    <div className="filter-container flex justify-between w-100% mt-10 mb-10 max-sm:w-100%">
                         <div className="filter-one">
-                            <h1>Color</h1>
+                            <p>Color: {product.color}</p>
                             <option></option>
                         </div>
                         <div className="filter-two">
@@ -65,13 +65,13 @@ function Product() {
                             </select>
                         </div>
                     </div>
-                    <div className="add-container flex items-center w-50% justify-between max-sm:w-100%">
+                    <div className="add-container flex items-center w-100% justify-between max-sm:w-100%">
                         <div className="amount-container flex items-center">
                             <button className="remove-btn" onClick={() => handleQuanitity('decrease')}>-</button>
                                 <p className="amount ml-2 mr-2">{quantity}</p>
                             <button className="add-btn" onClick={() => handleQuanitity('increase')}>+</button>
                         </div>
-                        <button onClick={handleClick}>ADD TO CART</button>
+                        <button className="border-4 p-2 ml-10" onClick={handleClick}>ADD TO CART</button>
                     </div>
                 </div>
             </div>
