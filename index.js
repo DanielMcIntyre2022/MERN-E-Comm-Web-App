baf5a39ea87cd98ef34474c1f6fb39cb58f77b72
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-23 18:01:20 -0500

    deployment testing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
 const mongoose = require('mongoose');
-const PORT = process.env.BACKENDPORT || PORT;
+const PORT = process.env.BACKENDPORT;
 const userRoute = require('./backend/routes/user');
 const authRoute = require('./backend/routes/auth');
 const productRoute = require('./backend/routes/product');
