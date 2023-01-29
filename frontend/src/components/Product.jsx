0191d10972326061316f35af748a466cddf1d7dc
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-29 18:51:45 -0500

    further Proudct component restructuring with tailwindcss styles

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom';
 
 function Product({item}) {
   return (
-    <div className="product-container flex-1 m-5 flex items-center justify-center mt-36 max-sm:mb-16">
-          <div className="product-image-container flex items-center justify-center w-56">
+    <div className="product-container flex-1 m-5 flex items-center justify-center mt-36">
+          <div className="product-image-container w-56 max-sm:-mb-20">
           <Link to={`/product/${item._id}`}>
-            <img className="h-60 w-100%" src={item.img}/>
+            <img className="h-60 w-56" src={item.img}/>
             <div className="info-container flex items-center relative justify-center">
               <p className='mt-5 font-bold'>${item.price}.00</p>
           </div>
