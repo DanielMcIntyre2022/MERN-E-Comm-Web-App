910f8c8c66d5c05d33069a3e65b39fa09c2fe041
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-29 15:30:23 -0500

    mobile testing - reszing and making height smaller for catergory item components

--- frontend/src/components/CatergoryItem.jsx
+++ frontend/src/components/CatergoryItem.jsx
@@ -6,10 +6,10 @@ function CatergoryItem({item}) {
   return (
     <div className="catergory-item-container flex-1 m-3 h-16 -mt-10 relative max-sm:pb-36">
         <Link to={`/products/${item.catergory}`}>
-            <div className="catergory-image-container max-sm:h-200">
-                <img className="w-100% h-80 object-cover" src={item.img}/>
+            <div className="catergory-image-container">
+                <img className="w-100% h-80 object-cover max-sm:h-36" src={item.img}/>
             </div>
-            <div className="catergory-info-container absolute max-sm:absolute w-100% h-full top-0 left-0 flex items-center justify-center flex-col ml-5 mt-5 font-bold p-10">
+            <div className="catergory-info-container absolute w-100% h-full top-0 left-0 flex items-center justify-center flex-col ml-5 mt-5 font-bold p-10">
                 <p className="text-white mb-1">{item.title}</p>
                 <button className="border-4 text-white p-2">SHOP NOW</button>
             </div>
