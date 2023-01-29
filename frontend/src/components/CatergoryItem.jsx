2159322bcd0d3feb312a3b202d6ff46b1d5a7706
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-29 15:12:34 -0500

    updates to mobile testing

--- frontend/src/components/CatergoryItem.jsx
+++ frontend/src/components/CatergoryItem.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 function CatergoryItem({item}) {
 
   return (
-    <div className="catergory-item-container flex-1 m-3 h-16 -mt-10 relative max-sm:pb-96">
+    <div className="catergory-item-container flex-1 m-3 h-16 -mt-10 relative">
         <Link to={`/products/${item.catergory}`}>
             <div className="catergory-image-container">
                 <img className="w-100% h-80 object-cover" src={item.img}/>
             </div>
-            <div className="catergory-info-container max-sm:absolute absolute w-100% h-full top-0 left-0 flex items-center justify-center flex-col ml-5 mt-5 font-bold p-10">
-                <p className="text-white mb-1 max-sm:absolute">{item.title}</p>
-                <button className="border-4 text-white p-2 max-sm:absolute max-sm:p-5">SHOP NOW</button>
+            <div className="catergory-info-container absolute w-100% h-full top-0 left-0 flex items-center justify-center flex-col ml-5 mt-5 font-bold p-10">
+                <p className="text-white mb-1">{item.title}</p>
+                <button className="border-4 text-white p-2">SHOP NOW</button>
             </div>
         </Link>
     </div>
