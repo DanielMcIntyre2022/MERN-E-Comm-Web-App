72e77847162a413c1f1c6bda9a7f20896e1a3dfc
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-01-30 14:26:47 -0500

    responsive adjustments to Register component

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -14,17 +14,17 @@ function Register() {
     <Navbar/>
     <div className="register-page-container w-screen h-screen flex items-center justify-center">
         <div className="register-wrapper w-40% ">
-            <h1 className="text-lg text-center">CREATE AN ACCOUNT</h1>
+            <h1 className="text-lg text-center max-sm:text-sm">CREATE AN ACCOUNT</h1>
             <form className="flex flex-col flex-wrap">
-                <input className="border-4 flex-1 mb-3 mt-3 p-2" placeholder="name"/>
-                <input className="border-4 flex-1 mb-3 mt-3 p-2" placeholder="last name"/>
-                <input className="border-4 flex-1 mb-3 mt-3 p-2" placeholder="email"/>
-                <input className="border-4 flex-1 mb-3 mt-3 p-2" placeholder="username"/>
-                <input className="border-4 flex-1 mb-3 mt-3 p-2" type='password' placeholder="password"/>
-                <input className="border-4 flex-1 mb-3 mt-3 p-2" type='password' placeholder="confirm password"/>
+                <input className="border-4 flex-1 mb-3 mt-3 p-2 max-sm:text-sm" placeholder="name"/>
+                <input className="border-4 flex-1 mb-3 mt-3 p-2 max-sm:text-sm" placeholder="last name"/>
+                <input className="border-4 flex-1 mb-3 mt-3 p-2 max-sm:text-sm" placeholder="email"/>
+                <input className="border-4 flex-1 mb-3 mt-3 p-2 max-sm:text-sm" placeholder="username"/>
+                <input className="border-4 flex-1 mb-3 mt-3 p-2 max-sm:text-sm" type='password' placeholder="password"/>
+                <input className="border-4 flex-1 mb-3 mt-3 p-2 max-sm:text-sm" type='password' placeholder="confirm password"/>
                 <div>
                   <div className='flex items-center justify-center'>
-                  <button onClick={handleClick} className="flex border-4 p-2">CREATE</button>
+                  <button onClick={handleClick} className="flex border-4 p-2 max-sm:text-sm">CREATE</button>
                   </div>
                 </div>
             </form>
