9e801b67a9ecbbdc0af66c006c2a4d38ef6aa149
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-01 11:25:46 -0500

    updates to Cart component

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -85,7 +85,7 @@ useEffect(() => {
                     </div>
                     <div className="flex">
                     <StripeCheckout
-                    name='E-Comm'
+                    name='E-Commerce'
                     image="https://images.unsplash.com/photo-1561715276-a2d087060f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
                     billingAddress
                     shippingAddress
